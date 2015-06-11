6578db7c18ca0b45b5a0d36d6a16f5010b128bbe
Tim Golen <tim@golen.net>
2015-06-10 21:05:16 -0600

    sign up from hero unit

--- content/themes/codefresh/assets/js/index.js
+++ content/themes/codefresh/assets/js/index.js
@@ -7,8 +7,25 @@
     $(document).on('ready', function(){
 
         // Handle our signup forms being submitted
-        $('.js-form-signup').on('submit', signUp);
+        $('.hero .js-form-signup').on('submit', signUpFromHero);
+        $('footer .js-form-signup').on('submit', signUpFromFooter);
+
+        $('.js-show-signup').click(showSignUp);
     });
+
+    function showSignUp(e) {
+        var $form = $(this).parent().find('form');
+        var $this = $(this);
+        e.preventDefault();
+
+        $this.fadeOut(function(){
+            $form.fadeIn(function(){
+                $form.find('input').first().focus();
+            });
+        });
+
+        return false;
+    }
 
     /**
      * Submit the signup form to the server
@@ -18,10 +35,54 @@
      * @date 2015-06-10
      *
      * @param {object} e jQuery submit form event
+     * @param {function} cb called only on success
      *
      * @return {boolean}
      */
-    function signUp(e) {
+    function signUp(e, cb) {
+        var $form = $(this);
+        var $name = $form.find('[name="FULLNAME"]');
+        var $email = $form.find('[name="EMAIL"]');
+
+        $.post('/ghost/api/v0.1/signup', {
+                name: $name.val(),
+                email: $email.val()
+            })
+            .done(cb)
+            .fail(function(xhr, msg, data){
+                alert('Server Error: ' + data);
+            });
+    }
+
+    function signUpFromHero(e) {
+        var $form = $(this);
+        var $signupButton = $('.js-show-signup');
+        var $alert = $form.parent().find('.alert');
+        var $name = $form.find('[name="FULLNAME"]');
+        var $email = $form.find('[name="EMAIL"]');
+
+        e.preventDefault();
+        
+        signUp(e, function(){
+            // Show our thank you message, then fade the form back in
+            // and clear it out
+            $form.fadeOut(function(){
+                $alert.fadeIn(function(){
+                    setTimeout(function(){
+                        $alert.fadeOut(function(){
+                            $name.val('');
+                            $email.val('');
+                            $signupButton.fadeIn();
+                        });
+                    }, 5000);
+                });
+            });
+        });
+
+        return false;
+    }
+
+    function signUpFromFooter(e) {
         var $form = $(this);
         var $alert = $form.parent().find('.alert');
         var $name = $form.find('[name="FULLNAME"]');
@@ -29,29 +90,22 @@
 
         e.preventDefault();
 
-        $.post('/ghost/api/v0.1/signup', {
-                name: $name.val(),
-                email: $email.val()
-            })
-            .done(function(){
-
-                // Show our thank you message, then fade the form back in
-                // and clear it out
-                $form.fadeOut(function(){
-                    $alert.fadeIn(function(){
-                        setTimeout(function(){
-                            $alert.fadeOut(function(){
-                                $name.val('');
-                                $email.val('');
-                                $form.fadeIn();
-                            });
-                        }, 5000);
-                    });
+        signUp(e, function(){
+            // Show our thank you message, then fade the form back in
+            // and clear it out
+            $form.fadeOut(function(){
+                $alert.fadeIn(function(){
+                    setTimeout(function(){
+                        $alert.fadeOut(function(){
+                            $name.val('');
+                            $email.val('');
+                            $form.fadeIn();
+                        });
+                    }, 5000);
                 });
-            })
-            .fail(function(xhr, msg, data){
-                alert('Server Error: ' + data);
             });
+        });
+
         return false;
     }
 })(jQuery);
