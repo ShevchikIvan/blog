28ce50bf23c2a746003a1d4b0b2808ca80c14f7f
Tim Golen <tim@golen.net>
2015-06-10 20:53:27 -0600

    code cleanup

--- content/themes/codefresh/assets/js/index.js
+++ content/themes/codefresh/assets/js/index.js
@@ -5,12 +5,27 @@
 /* globals jQuery, document */
 (function ($) {
     $(document).on('ready', function(){
-        $('footer .js-form-signup').on('submit', signUp);
+
+        // Handle our signup forms being submitted
+        $('.js-form-signup').on('submit', signUp);
     });
 
+    /**
+     * Submit the signup form to the server
+     *
+     * @author Tim Golen <tim@golen.net>
+     *
+     * @date 2015-06-10
+     *
+     * @param {object} e jQuery submit form event
+     *
+     * @return {boolean}
+     */
     function signUp(e) {
-        var $name = $('footer .js-form-signup [name="FULLNAME"]');
-        var $email = $('footer .js-form-signup [name="EMAIL"]');
+        var $form = $(this);
+        var $alert = $form.parent().find('.alert');
+        var $name = $form.find('[name="FULLNAME"]');
+        var $email = $form.find('[name="EMAIL"]');
 
         e.preventDefault();
 
@@ -19,13 +34,16 @@
                 email: $email.val()
             })
             .done(function(){
-                $('footer .js-form-signup').fadeOut(function(){
-                    $('footer .alert').fadeIn(function(){
+
+                // Show our thank you message, then fade the form back in
+                // and clear it out
+                $form.fadeOut(function(){
+                    $alert.fadeIn(function(){
                         setTimeout(function(){
-                            $('footer .alert').fadeOut(function(){
+                            $alert.fadeOut(function(){
                                 $name.val('');
                                 $email.val('');
-                                $('footer .js-form-signup').fadeIn();
+                                $form.fadeIn();
                             });
                         }, 5000);
                     });
